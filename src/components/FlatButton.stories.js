87c028ea39edd1b158bcf61cd3c7ca0bc0035d71
Callum Gare <cgare@infoxchange.org>
2021-02-25 21:41:21 +1100

    Add initial zeplin integration

--- src/components/FlatButton.stories.js
+++ src/components/FlatButton.stories.js
@@ -4,6 +4,9 @@ import FlatButton from "./FlatButton";
 export default {
     title: "Basic UI Components/FlatButton",
     component: FlatButton,
+    parameters: {
+        zeplinLink: "https://app.zeplin.io/project/603770b45ed7b30183b01a07/styleguide/components?coid=6037734317b4e8270a6cef9c",
+    },
 };
 
 const Template = (args) => <FlatButton {...args} />;
